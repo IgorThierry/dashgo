42e464235ce0ed28802bc94a864def957a286202
Igor Thierry <igorthierry15@gmail.com>
2021-07-12 17:16:03 -0300

    desaclopando func getUsers

--- src/services/hooks/users/useUsers.ts
+++ src/services/hooks/users/useUsers.ts
@@ -1,27 +1,32 @@
 import { useQuery } from 'react-query';
 import { api } from '../../api';
 
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+};
+
+export async function getUsers(): Promise<User[]> {
+  const { data } = await api.get('/users');
+
+  const users = data.users.map(user => {
+    return {
+      ...user,
+      createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+    };
+  });
+
+  return users;
+}
+
 export function useUsers() {
-  return useQuery(
-    'users',
-    async () => {
-      const { data } = await api.get('/users');
-
-      const users = data.users.map(user => {
-        return {
-          ...user,
-          createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-            day: '2-digit',
-            month: 'long',
-            year: 'numeric',
-          }),
-        };
-      });
-
-      return users;
-    },
-    {
-      staleTime: 1000 * 5, // 5s
-    },
-  );
+  return useQuery('users', getUsers, {
+    staleTime: 1000 * 5, // 5s
+  });
 }
